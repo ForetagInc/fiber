b6ac706c17530b0f9a133cf7e02e2e150d1f57bf
Chiru B <chirub@foretag.co>
2022-04-30 15:13:58 +0100

    add: Firefox configuration

---
+++ firefox/autoconfig.js
@@ -0,0 +1,8 @@
+pref("general.config.filename", "firefox.cfg");
+pref("general.config.obscure_value", 0);
+
+// Updates
+lockPref("app.update.auto", true);
+lockPref("app.update.enabled", true);
+
+lockPref("browser.startup.homepage", "https://you.com");
